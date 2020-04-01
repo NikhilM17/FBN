4208788c6be6f3f7b216b873fce897c724882728
NikhilM17 <nikhil.masham@gmail.com>
2020-04-01 13:30:56 +0530

    adding listener

--- notifications/src/main/java/com/example/notifications/MyFirebaseService.java
+++ notifications/src/main/java/com/example/notifications/MyFirebaseService.java
@@ -7,11 +7,18 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseService extends FirebaseMessagingService {
 
+    FirebaseMessageListener listener;
+
+    public void setListener(FirebaseMessageListener listener) {
+        this.listener = listener;
+    }
+
     BaseApp app = (BaseApp) getApplication();
 
     @Override
     public void onNewToken(@NonNull String s) {
         app.token(s);
+        listener.onNewToken(s);
     }
 
     @Override
