dd21ec0ca20505afc495c632606cd222efc990ef
NikhilM17 <nikhil.masham@gmail.com>
2020-04-03 13:35:04 +0530

    changes for notification action service

--- notifications/src/main/java/com/example/notifications/NotificationFirebaseService.java
+++ notifications/src/main/java/com/example/notifications/NotificationFirebaseService.java
@@ -7,7 +7,7 @@ import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class MyFirebaseService extends FirebaseMessagingService {
+public class NotificationFirebaseService extends FirebaseMessagingService {
 
     @Override
     public void onNewToken(@NonNull String s) {
